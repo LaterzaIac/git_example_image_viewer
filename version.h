8bdc56aef61f0daea4294b7a89a3a4ceb997ba56
Laterzaiac <laterza.iacopo@gmail.com>
2022-04-06 15:52:43 +0200

    Aggiunta version.h

---
+++ version.h
@@ -0,0 +1,10 @@
+//
+// Created by later on 06/04/2022.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+//non ricordo cosa dovevo mettere
+
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
