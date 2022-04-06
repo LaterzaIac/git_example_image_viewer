767458cb880018e7aff37f25a1792b5a85d421e4
Laterzaiac <laterza.iacopo@gmail.com>
2022-04-06 16:00:03 +0200

    BugFix #bug_2345 #bug_1234

--- BitmapImage.cpp
+++ BitmapImage.cpp
@@ -7,7 +7,7 @@ BitmapImage::BitmapImage() : width(640), height(480), channel(3), depht(8) {
 bool BitmapImage::load(std::string name) {
     // check it is a supported image file
     std::string fileExt = name.substr(name.find_last_of(".") + 1);
-    if (fileExt == "jpg" || fileExt == "png" || fileExt == "bmp") {
+    if (fileExt == "jpg" || fileExt == "jpeg" || fileExt == "png" || fileExt == "bmp") {
         // load image file
         // decompress into buffer
         return true;
@@ -28,6 +28,9 @@ bool BitmapImage::save() {
 }
 
 void BitmapImage::resize(int width, int height) {
-    this->width = width;
-    this->height = height;
+    if (width <= 0 || height <= 0) {
+        this->width = width;
+        this->height = height;
+    }
+
 }
